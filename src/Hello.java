60e66fec6a7aac1d355e5cccd27ee210a770973c
dmodliba <dmodliba@student.tgm.ac.at>
2017-02-20 10:49:35 +0100

    Hallo Englisch 2

--- src/Hello.java
+++ src/Hello.java
@@ -2,11 +2,11 @@
 public class Hello {
 
 	public static void daniel(){
-		System.out.println("Hello Daniel");
+		System.out.println("Hello Daniel 2");
 	}
 	
 	public static void yasin(){
-		System.out.println("Hello Yasin");
+		System.out.println("Hello Yasin 2");
 	}
 	
 }
