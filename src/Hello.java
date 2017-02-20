6310564178e44c4e2ba2834d3f23cd8de83099dd
dmodliba <dmodliba@student.tgm.ac.at>
2017-02-20 10:49:11 +0100

    Hallo Polnisch 2

--- src/Hello.java
+++ src/Hello.java
@@ -2,11 +2,11 @@
 public class Hello {
 
 	public static void daniel(){
-		System.out.println("Czesc Daniel");
+		System.out.println("Czesc Daniel 2");
 	}
 	
 	public static void yasin(){
-		System.out.println("Czesc Yasin");
+		System.out.println("Czesc Yasin 2");
 	}
 	
 }
