53547b31fba502d8151dbd08072ce56056a5b9fe
dmodliba <dmodliba@student.tgm.ac.at>
2017-02-20 11:10:17 +0100

    WEiser

--- src/Hello.java
+++ src/Hello.java
@@ -2,11 +2,11 @@
 public class Hello {
 
 	public static void daniel(){
-		System.out.println("Hello Daniel 2");
+		System.out.println("Hello Daniel 3");
 	}
 	
 	public static void yasin(){
-		System.out.println("Hello Yasin 2");
+		System.out.println("Hello Yasin 3");
 	}
 	
 }
